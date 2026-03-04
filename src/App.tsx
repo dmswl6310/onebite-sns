5fe47a2866ea5dd099d7be5a078bbd88ac4d534e
dmswl6310 <dmswl6310@gmail.com>
2026-03-04 23:45:37 +0900

    setting: tailwindcss 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      {/* 1. 타이포그래프 */}
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-2xl font-black">text-2xl</div>
+      <div className="text-[13px]">text-13px</div>
+
+      {/* 2. 백그라운드컬러 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className="w-20 bg-blue-500">box</div>
+      <div className="w-[20px] bg-blue-500">box</div>
+      <div className="h-20 w-full bg-blue-500">box</div>
+
+      {/* 4. 여백 */}
+      <div className="m-5 h-50 w-50 bg-red-400 px-5 pt-5 pb-5">
+        <div className="h-full w-full bg-blue-400"></div>
+      </div>
+
+      {/* 5. 보더 */}
+      <div className="border-2">border</div>
+      <div className="m-5 rounded-md border-x-2 border-l-2 border-red-500">
+        border
+      </div>
+
+      {/* 6. 플렉스 컨테이너 */}
+      <div className="flex flex-row items-center justify-center">
+        <div className="h-10 w-10 border">1</div>
+        <div className="h-20 w-10 flex-1 border">2</div>
+        <div className="h-30 w-10 border">3</div>
+        <div className="h-40 w-10 border">4</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
