b684dd2d7d54d7a9b1ee7cf10bce2dfe03ea0d89
dmswl6310 <dmswl6310@gmail.com>
2026-03-10 15:54:03 +0900

    feat: zustand combine 미들웨어 사용

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,34 +1,50 @@
 import { create } from "zustand"; // create = store(zustand에서 state 와 action 함수가 포함된 객체) 생성
+import { combine } from "zustand/middleware";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
-
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      /*
+export const useCountStore = create(
+  combine({ count: 0 }, (set, get) => ({
+    actions: {
+      increase: () => {
+        /*
     // 아래와 같음
     const count = get().count;
     set({ count: count + 1 }); // 명시된 property 값만 업데이트
     */
 
-      set((store) => ({
-        count: store.count + 1,
-      }));
+        set((state) => ({
+          count: state.count + 1,
+        }));
+      },
+      decrease: () => {
+        set((state) => ({
+          count: state.count - 1,
+        }));
+      },
     },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+  })),
+);
+
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       /*
+//     // 아래와 같음
+//     const count = get().count;
+//     set({ count: count + 1 }); // 명시된 property 값만 업데이트
+//     */
+
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
