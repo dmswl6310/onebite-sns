10106ab7f70052ac311fd1452b75ca519c638ae7
dmswl6310 <dmswl6310@gmail.com>
2026-03-19 19:10:32 +0900

    feat: zustand 미들웨어 devtools적용

--- src/store/count.ts
+++ src/store/count.ts
@@ -4,35 +4,41 @@ import {
   subscribeWithSelector,
   persist,
   createJSONStorage,
+  devtools,
 } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useCountStore = create(
-  persist(
-    subscribeWithSelector(
-      immer(
-        combine({ count: 0 }, (set, get) => ({
-          actions: {
-            increase: () => {
-              set((state) => {
-                state.count += 1;
-              });
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
             },
-            decrease: () => {
-              set((state) => {
-                state.count -= 1;
-              });
-            },
-          },
-        })),
+          })),
+        ),
       ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
+      },
     ),
     {
       name: "countStore",
-      partialize: (store) => ({
-        count: store.count,
-      }),
-      storage: createJSONStorage(() => sessionStorage),
     },
   ),
 );
