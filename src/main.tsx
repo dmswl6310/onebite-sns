638d07e7e7af05e7042c4a91d7cd07c921d7ddce
dmswl6310 <dmswl6310@gmail.com>
2026-03-20 12:00:21 +0900

    chore: tanstack query 설치 및 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(); // queryClient는 일종의 store(api요청응답값, 캐싱)
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
