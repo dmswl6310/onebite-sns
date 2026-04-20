6b2e5e2ae269f677abc9cdf60f941a10ab2bd86d
dmswl6310 <dmswl6310@gmail.com>
2026-04-20 11:49:10 +0900

    feat: tanstack query 캐시 옵션 사용

--- src/hooks/queries/use-todo-data-by-id.ts
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -5,5 +5,13 @@ export function useTodoDataById(id: number) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
     queryKey: ["todos", id],
+
+    staleTime:5000,
+    gcTime:5000,
+
+    // refetchOnMount:false,
+    // refetchOnWindowFocus:false,
+    // refetchOnReconnect:false,
+    // refetchInterval:false,
   });
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,21 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-const queryClient = new QueryClient(); // queryClient는 일종의 store(api요청응답값, 캐싱)
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:0, // 주로 statleTime은 개발도중 0으로 함
+      gcTime:5*60*1000, // 주로 5분
+
+      refetchOnMount:true,
+      refetchOnWindowFocus:false,
+      refetchOnReconnect:false,
+      refetchInterval:false,
+    }
+  }
+  }
+); // queryClient는 일종의 store(api요청응답값, 캐싱)
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
