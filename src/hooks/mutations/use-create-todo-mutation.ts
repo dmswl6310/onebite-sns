71b36cfb3071d531fc4f6d6fceacc509a47185be
dmswl6310 <dmswl6310@gmail.com>
2026-04-21 12:00:47 +0900

    fix: 반환값 활용방식으로 변경 - 서버부하 최소화

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constant";
+import type { Todo } from "@/type";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -9,9 +10,10 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {}, // 요청이 시작되었을때
     onSettled: () => {}, // 요청이 종료되었을때
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     onError: (error) => {
