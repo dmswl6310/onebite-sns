e6b82a89c44ca0c897a1ea31ba64c7dcbb05d64f
dmswl6310 <dmswl6310@gmail.com>
2026-04-27 11:45:34 +0900

    feat: 수정에 대한 mutation과 예외 상황에 대해 필요한 옵션 추가

--- src/hooks/mutations/use-update-todo-mutation.ts
+++ src/hooks/mutations/use-update-todo-mutation.ts
@@ -1,8 +1,47 @@
 import { updateTodo } from "@/api/update-todo";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constant";
+import type { Todo } from "@/type";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdateTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updateTodo,
+    onMutate: async (updatedTodo) => {
+      // 예외2
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.list, // 해당 키에 대한 조회요청을 모두 취소시킴
+      });
+
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [];
+        return prevTodos.map((prevTodo) =>
+          prevTodo.id == updatedTodo.id
+            ? { ...prevTodo, ...updatedTodo }
+            : prevTodo,
+        );
+      });
+      return {
+        prevTodos,
+      };
+    },
+
+    // 예외1
+    onError: (error, variable, context) => {
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos,
+        );
+      }
+    },
+    // 예외3
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+    },
   });
 }
