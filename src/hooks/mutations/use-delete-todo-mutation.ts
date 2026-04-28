4fe7c8af9667853f16264c07f0969096c1af0465
dmswl6310 <dmswl6310@gmail.com>
2026-04-28 10:10:37 +0900

    feat: 성공 시 응답값 활용하여 ui업데이트

--- src/hooks/mutations/use-delete-todo-mutation.ts
+++ src/hooks/mutations/use-delete-todo-mutation.ts
@@ -1,8 +1,24 @@
 import { deleteTodo } from "@/api/delete-todo";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constant";
+import type { Todo } from "@/type";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteTodo,
+    onSuccess: (deletedTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [];
+        return prevTodos.filter((prevTodo) => prevTodo.id !== deletedTodo.id);
+      });
+    },
   });
+
+  // 1. 캐시 무효화 => invalidateQueries
+  // 2. 수정 요청의 응답값 활용 => onSuccess
+  // (삭제 도중 체크 시 문제될수 있지만, 충분히 처리가능) => 요 방식으로 픽!
+  // 3. 낙관적 업데이트 => onMutate
+  // (삭제 후 화면에서 사라졌다가, 실패시 다시 보였을 때 이상함)
 }
