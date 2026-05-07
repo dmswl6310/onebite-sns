f9e2c8554b4c841bff0061d196304d7dd003e067
dmswl6310 <dmswl6310@gmail.com>
2026-05-07 11:29:08 +0900

    feat: 소셜로그인 기능 추가

---
+++ src/hooks/mutations/use-sign-in-with-oauth.ts
@@ -0,0 +1,6 @@
+import { signInWithOAuth } from "@/api/auth";
+import { useMutation } from "@tanstack/react-query";
+
+export function useSignInWithOAuth() {
+  return useMutation({ mutationFn: signInWithOAuth });
+}
--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -3,12 +3,15 @@ import { Input } from "@/components/ui/input";
 import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
 import { useState } from "react";
 import { Link } from "react-router";
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAuth } = useSignInWithOAuth();
 
   const handleSignInWithPasswordClick = () => {
     if (email.trim() === "") return;
@@ -19,6 +22,11 @@ export default function SignInPage() {
       password,
     });
   };
+
+  const handleSignInWithGithubClick = () => {
+    signInWithOAuth("github");
+  };
+
   return (
     <div className="flex flex-col gap-8">
       <div className="text-xl font-bold">로그인</div>
@@ -38,9 +46,17 @@ export default function SignInPage() {
           placeholder="password"
         />
       </div>
-      <div>
+      <div className="flex flex-col gap-2">
         <Button onClick={handleSignInWithPasswordClick} className="w-full">
           로그인
+        </Button>
+        <Button
+          onClick={handleSignInWithGithubClick}
+          className="w-full"
+          variant={"outline"}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인
         </Button>
       </div>
       <div>
